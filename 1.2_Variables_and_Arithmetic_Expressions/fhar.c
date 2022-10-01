6ace56294b9c6b1bffa6c6967e69ffad1caa5de1
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-01 14:01:58 +0200

    test

--- 1.2_Variables_and_Arithmetic_Expressions/fhar.c
+++ 1.2_Variables_and_Arithmetic_Expressions/fhar.c
@@ -5,17 +5,17 @@
 */
 int main (void)
 {
-	int fahr, celsius;
-	int  lower, upper,step ;
+	float fahr, celsius;
+	float  lower, upper,step ;
 	
 	lower = 0;
 	upper = 300;
 	step = 20;
-
+	fahr = lower;
 	while (fahr <= upper)
 		{
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 		}
 }
