4261c69d01b3998f92e0a9befe13c1f2dd5e67dd
uoch <uoch@LAPTOP-S9NJHPLK.localdomain>
2022-10-01 15:11:17 +0200

    paste

--- 1.2_Variables_and_Arithmetic_Expressions/second fahr.c
+++ 1.2_Variables_and_Arithmetic_Expressions/second fahr.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit */
+#define STEP 20 /* step size */
 /* print Fahrenheit-Celsius table */
-int main(void)
+int main ()
 {
 int fahr;
 for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+printf("%6.1f %3d\n",(5.0/9.0)*(fahr-32), fahr );
 return (0);
 }
